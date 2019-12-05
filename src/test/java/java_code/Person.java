f21c3f6986bb1d97ca665ad3131a9658b667cf48
Dinara Hudaykulyyeva <hcdinara13@gmail.com>
2019-12-05 13:46:26 -0600

    first commit

---
+++ src/test/java/java_code/Person.java
@@ -0,0 +1,14 @@
+package java_code;
+
+public class Person {
+
+    private int age;
+
+
+    public Person(int age){
+
+        this.age=age;
+
+    }
+
+}
---
+++ src/test/java/java_code/Student.java
@@ -0,0 +1,11 @@
+package java_code;
+
+public class Student extends Person{
+
+    public Student(int age) {
+
+        super(age);
+
+    }
+
+}
